36f372021bda2d13a2f947b663e0e975b697480f
Prasanna Sriram <pras306@gmail.com>
2019-02-12 03:39:18 +0530

    Removed unwawnted commented code

--- Numbers/change_return.py
+++ Numbers/change_return.py
@@ -100,31 +100,4 @@ def clear_display():
 clear_button = Button(frame, width=10,text='Clear', command=clear_display)
 clear_button.grid(row=6,column=1)
 
-# while cost > amount_given:
-#     print("User has not paid the entire amount.")
-#     cost = float(input("Enter the total cost (in dollars): "))
-
-# change_amount = round(amount_given - cost, 2) 
-
-# print("The change to be returned is {0}".format(change_amount))
-
-# cents = round(change_amount % 1, 2)
-
-# while(cents > 0):
-#     if cents >= 0.25:
-#         quarters += 1
-#         cents = round(cents - 0.25, 2)
-#     elif cents < 0.25 and cents >= 0.10:
-#         dimes += 1
-#         cents = round(cents - 0.10, 2)
-#     elif cents < 0.10 and cents >= 0.05:
-#         nickels += 1
-#         cents = round(cents - 0.05, 2)
-#     else:
-#         pennies += 1
-#         cents = round(cents - 0.01, 2)        
-    
-# print("The number of quarters is {0}, dimes is {1}, nickels is {2} and pennies is {3}".format(quarters, dimes, nickels, pennies))
-
-
 root.mainloop()
